618eb040cc4016c5fe798ae90050921947cdee39
Mats Estensen <mats.est@gmail.com>
2020-09-28 16:46:58 +0200

    add initial version

--- tfc.ps1
+++ tfc.ps1
@@ -2,31 +2,35 @@
 #### requires ps-version 7.0 ####
 <#
 .SYNOPSIS
-Script to get TFE/TFC workspace info to check version and also to trigger the workpaces
+API experiment to get TFC workspaces info and trigger a workspace run
 .DESCRIPTION
-The script will allow you to get you TFC/TFE Organizations workspace Information and 
-trigger multiple workspaces in parallel  if required by making API calls 
+Lists the workspace information and allows you to interactively trigger a run.
 .PARAMETER <org>
 <Organization name>
 .PARAMETER <TFE_TOKEN>
-<Terraform Token>
+<Terraform Token> - defaults to the $TF_TOKEN environment variable
 .PARAMETER <Server>
-<Terraform Cloud/Enterprise Sever name >
+<Terraform Cloud/Enterprise Server name > - defaults to app.terraform.io
 .NOTES
+   Version:        0.2
+   Author:         Mats Estensen
+   Creation Date:  Tuesday, September 28th 2020
+   File: tfc.ps1
+
    Version:        0.1
    Author:         John Alfaro
    Creation Date:  Saturday, July 25th 2020, 11:10:44 pm
    File: tf.ps1
    Copyright (c) 2020 jalfaro
 HISTORY:
-Date      	          By	Comments
-----------	          ---	----------------------------------------------------------
+Date      	          By	          Comments
+----------	          ---	          ----------------------------------------------------------
+28.09.2020            Mats Estensen   See github.com/matsest/tfc-pwsh for changes
+25.07.2020            John Alfaro     Initial version
 
 .LINK
    https://jalfaro.blog.com
-
-.COMPONENT
- Required Modules: 
+   https://github.com/matsest/tfc-pwsh
 
 .LICENSE
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -46,18 +50,24 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
- 
+
 .EXAMPLE
-.\tf.ps1 -Org  "CerocoolCorp" -Server "app.terraform.io" -tfe_token "fdsgfhfhtrhs4t546567egbgl"
+# Using the default org (app.terraform.io) and having TF_TOKEN set as an env variable
+.\tfc.ps1 -Org "myorg"
+
+.EXAMPLE
+# Passing all the variables explictily (not recommended)
+.\tfc.ps1 -Org "myorg" -Server "app.terraform.io" -TF_TOKEN $MY_TF_TOKEN
 #>
+
 [cmdletBinding()]
 Param(
     [Parameter(Mandatory = $true)]
     [string] $Org,
-    [Parameter(Mandatory = $true)]
-    [string] $Server,
-    [Parameter(Mandatory = $true)]
-    [string] $TF_TOKEN
+    [Parameter(Mandatory = $false)]
+    [string] $Server = "app.terraform.io",
+    [Parameter(Mandatory = $false)]
+    [string] $TF_TOKEN = $env:TF_TOKEN
 )
 
 $pag = 1
@@ -73,17 +83,17 @@ $headers = @{
 try {
     Write-Verbose -Message "connecting and getting workspace info from TFC/TFE using token"
     #Getting the numnber of pages used in TFC/TFE
-    $page = (Invoke-RestMethod  -Uri "https://$Server/api/v2/organizations/$Org/workspaces?page%5Bnumber=$pag%5D" -Method Get -ContentType "application/vnd.api+json" -Headers $headers -ErrorVariable $ErrorCredential).meta.pagination.'total-pages' 
-    While ($pag -le $page) {
+    $pages = (Invoke-RestMethod  -Uri "https://$Server/api/v2/organizations/$Org/workspaces?page%5Bnumber=$pag%5D" -Method Get -ContentType "application/vnd.api+json" -Headers $headers -ErrorVariable $ErrorCredential).meta.pagination.'total-pages' 
+    While ($pag -le $pages) {
         #getting all the workspace information on the organization
         $data = (Invoke-RestMethod  -Uri "https://$Server/api/v2/organizations/$Org/workspaces?page%5Bnumber=$pag%5D" -Method Get -ContentType "application/vnd.api+json" -Headers $headers).data
 
+        Write-Verbose "Getting Terraform Workspaces"
         foreach ($workspace in $data) {
             $workspaceID = $workspace.id
-            Write-Progress -Activity "getting Terraform Workspaces" -Status "Working on worksapce $workspace"  -PercentComplete ((($data.IndexOf($workspace)) / $data.Count) * 100)
-            Write-Progress -Activity "Terraform Workspaces" -Status "Done" -PercentComplete 100 -Completed
+            Write-Verbose (@("Getting workspace {0}/{1} on page {2}/{3}" -f $data.Indexof($workspace), $data.Count, $pag, $pages) | Out-String)
             #This will get the state information of the workspaces
-            $status = (Invoke-RestMethod  -Uri "https://$Server/api/v2/workspaces/$workspaceID/runs" -Method Get -ContentType "application/vnd.api+json" -Headers $headers).data.attributes.status[0]
+            $status = (Invoke-RestMethod  -Uri "https://$Server/api/v2/workspaces/$workspaceID/runs" -Method Get -ContentType "application/vnd.api+json" -Headers $headers).data.attributes.status
             $wrk = new-object PSObject
             $wrk | add-member -MemberType NoteProperty -Name "WorkspaceID" -Value $workspace.id
             $wrk | add-member -MemberType NoteProperty -Name "WorkspaceName" -Value $workspace.attributes.name
@@ -103,15 +113,16 @@ catch {
     break
 }
  
-$instances = ($workspaces | Out-GridView -OutputMode Multiple -Title ‘Please select the workspace/worspaces to run.’).WorkspaceID 
-$message = Read-Host "Message for this Workpsace run"
-$instances | ForEach-Object -parallel {
+$workspaces | Out-Host
+$selectedWorkspaceID = Read-Host "Enter a workspace ID to trigger a run for"
+$selectedWorkspaceName = ($workspaces | Where-Object -Property WorkSpaceID -eq $selectedWorkspaceID).WorkspaceName
+$selectedWorkspaceID | ForEach-Object -Parallel {
     $body = @"
     {
         "data": {
             "attributes": {
                 "is-destroy": false,
-                "message": "$using:message"
+                "message": "triggered from Powershell"
             },
             "type": "runs",
             "relationships": {
@@ -127,9 +138,10 @@ $instances | ForEach-Object -parallel {
     }
 "@
  
-    #This will attempt to trigger the run on all selected workspaces
+    # Trigger run for selected workspace
     try {
-        Invoke-RestMethod  -Uri "https://$using:server/api/v2/runs" -Method POST -ContentType "application/vnd.api+json" -Headers $using:headers -Body $body -ErrorVariable $ErrorCredential | Out-Null
+        $runId = (Invoke-RestMethod  -Uri "https://$using:Server/api/v2/runs" -Method POST -ContentType "application/vnd.api+json" -Headers $using:headers -Body $body -ErrorVariable $ErrorCredential).data.id
+        "Success! See the run here: https://$using:Server/app/$using:Org/workspaces/$using:selectedWorkspaceName/runs/$runId"
     }
     catch {
         IF ($ErrorCredential) {
