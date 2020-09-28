89fdc30710578144e6f6a261a7c3455a4ca26e8b
Mats Estensen <mats.est@gmail.com>
2020-09-28 17:18:15 +0200

    update comment based help

--- tfc.ps1
+++ tfc.ps1
@@ -1,55 +1,61 @@
-<#
-#### requires ps-version 7.0 ####
+#Requires -Version 7.0
+
 <#
 .SYNOPSIS
 API experiment to get TFC workspaces info and trigger a workspace run
+
 .DESCRIPTION
 Lists the workspace information and allows you to interactively trigger a run.
-.PARAMETER <org>
-<Organization name>
-.PARAMETER <TFE_TOKEN>
-<Terraform Token> - defaults to the $TF_TOKEN environment variable
-.PARAMETER <Server>
-<Terraform Cloud/Enterprise Server name > - defaults to app.terraform.io
-.NOTES
-   Version:        0.2
-   Author:         Mats Estensen
-   Creation Date:  Tuesday, September 28th 2020
-   File: tfc.ps1
 
-   Version:        0.1
-   Author:         John Alfaro
-   Creation Date:  Saturday, July 25th 2020, 11:10:44 pm
-   File: tf.ps1
-   Copyright (c) 2020 jalfaro
-HISTORY:
-Date      	          By	          Comments
-----------	          ---	          ----------------------------------------------------------
-28.09.2020            Mats Estensen   See github.com/matsest/tfc-pwsh for changes
-25.07.2020            John Alfaro     Initial version
+.PARAMETER Org
+The TFE/TFC organization name.
+
+.PARAMETER Server
+The TFE/TFC server. Defaults to app.terraform.io.
+
+.PARAMETER TF_TOKEN
+The token used to authenticate to TFC/TFE. Defaults to the environment variable TF_TOKEN.
+
+.NOTES
+    Version:        0.2
+    Author:         Mats Estensen
+    Creation Date:  Tuesday, September 28th 2020
+    File: tfc.ps1
+    Version:        0.1
+    Author:         John Alfaro
+    Creation Date:  Saturday, July 25th 2020, 11:10:44 pm
+    File: tf.ps1
+    Copyright (c) 2020 jalfaro
+    
+    HISTORY
+    Date      	          By	          Comments
+    ----------	          ---	          ----------------------------------------------------------
+    28.09.2020            Mats Estensen   See github.com/matsest/tfc-pwsh for changes
+    25.07.2020            John Alfaro     Initial version
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the Software), to deal
+    in the Software without restriction, including without limitation the rights
+    to use copy, modify, merge, publish, distribute sublicense and /or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 
 .LINK
-   https://jalfaro.blog.com
-   https://github.com/matsest/tfc-pwsh
+https://jalfaro.blog.com
 
-.LICENSE
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the Software), to deal
-in the Software without restriction, including without limitation the rights
-to use copy, modify, merge, publish, distribute sublicense and /or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+.LINK
+https://github.com/matsest/tfc-pwsh
 
 .EXAMPLE
 # Using the default org (app.terraform.io) and having TF_TOKEN set as an env variable
@@ -62,7 +68,7 @@ SOFTWARE.
 
 [cmdletBinding()]
 Param(
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $true, HelpMessage = "Enter the name of the TFC organization")]
     [string] $Org,
     [Parameter(Mandatory = $false)]
     [string] $Server = "app.terraform.io",
